e9169a971650ae5e484f716f727dc27cdf36fc2d
Cameron Mackenzie <cameron@localhost.localdomain>
2019-07-13 16:03:00 +0100

    see previous commit

--- board.py
+++ board.py
@@ -3,8 +3,8 @@ class Board:
     def __init__(self,dimensions=[3,3]):
         self.cells=numpy.empty(dimensions)
 
-    def placeMove(self,x,y):
-        pass
+    def placeMove(self,coordinates,value):
+        self.cells.itemset(coordinates,value) # coordinates has to be passed as a tuple
 
-    def checkWon(self):
+    def checkWon(self,nInARow=3):
         pass
