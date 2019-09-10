3f3e0a23ff2bb511a31afb8e2e75ee30a9dbc014
Cameron Mackenzie <cameron@localhost.localdomain>
2019-09-10 20:04:54 +0100

    added x by y board

--- kivy test/mg.py
+++ kivy test/mg.py
@@ -1,18 +1,25 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 Builder.load_file("mg.kv")
 class MenuScreen(Screen):
     pass
 
 class SettingsScreen(Screen):
-    pass
+    h=3
+    w=4
+    grid=ObjectProperty(None)
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(SettingsScreen(name='settings'))
-
+a=SettingsScreen(name='settings')
+for y in range(SettingsScreen.h):
+    for x in range(SettingsScreen.w):
+        a.grid.add_widget(Button(text="{} . {}".format(x,y)))
+sm.add_widget(a)
 class TestApp(App):
 
     def build(self):
