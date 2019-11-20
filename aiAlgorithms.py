e8ec0b1318b5f2cec97a187a000fa6c18500289c
Cameron Mackenzie <ckameron99@gmail.com>
2019-11-20 14:51:22 +0000

    Implemented a more advanced abpmm algorithm, which preferes a long game when unable to force a win and a short game when able to force a win

--- aiAlgorithms.py
+++ aiAlgorithms.py
@@ -200,6 +200,101 @@ class NABPMM:
         #print(depth,val,self.board.cells,"\n")
         return bestMoveValue
 
+class ABPMM:
+    def __init__(self,board):
+        self.playerIndex=None
+        self.board=board
+    def getMove(self,playerIndex):
+        maxDepth=-1
+        for index, value in numpy.ndenumerate(self.board.cells):
+            if self.board.cells[index]=="0.0":
+                maxDepth+=1
+        self.maxDepth=maxDepth
+        self.winMoveDepth=maxDepth
+        self.loseMoveDepth=0
+        self.playerIndex=playerIndex
+        bestMoveValue,bestMoveLocation=-inf,None
+        for index,value in numpy.ndenumerate(self.board.cells):
+            if self.board.cells[index]=="0.0":
+                self.board.cells[index]=self.board.players[self.playerIndex].value
+                val,moveDepth=self.abpmm(maxDepth,(self.playerIndex+1)%len(self.board.players),-inf,inf)
+                self.board.cells[index]="0.0"
+                if val>=bestMoveValue:
+                    if val==-1 or val == 0:
+                        if moveDepth>=self.loseMoveDepth:
+                            self.bestMoveValue=val
+                            self.bestMoveLocation=index
+                            self.loseMoveDepth=moveDepth
+                    elif val==1:
+                        if moveDepth<self.winMoveDepth:
+                            self.bestMoveValue=val
+                            self.bestMoveLocation=index
+                            self.winMoveDepth=moveDepth
+        return self.bestMoveLocation
+
+
+    def abpmm(self,depth,playerIndex,alpha,beta):
+        winMoveDepth=self.maxDepth
+        loseMoveDepth=0
+        for player in self.board.players:
+            if self.board.checkWin(value=self.board.players[playerIndex].value):
+                if playerIndex==self.playerIndex:
+                    return 1,0
+                else:
+                    return -1,0
+        if depth==0:
+            return 0,0
+        if playerIndex==self.playerIndex:
+            bestMoveValue=-inf
+            for index,value in numpy.ndenumerate(self.board.cells):
+                if self.board.cells[index]=="0.0":
+                    self.board.cells[index]=self.board.players[playerIndex].value
+                    val,moveDepth=self.abpmm(depth-1,(playerIndex+1)%len(self.board.players),alpha,beta)
+                    self.board.cells[index]="0.0"
+                    if val>=bestMoveValue:
+                        if val==-1 or val == 0:
+                            if moveDepth>=loseMoveDepth:
+                                bestMoveValue=val
+                                bestMoveLocation=index
+                                loseMoveDepth=moveDepth
+                                bestMoveDepth=loseMoveDepth
+                        elif val==1:
+                            if moveDepth<=winMoveDepth:
+                                bestMoveValue=val
+                                bestMoveLocation=index
+                                winMoveDepth=moveDepth
+                                bestMoveDepth=winMoveDepth
+                    alpha=max(alpha,bestMoveValue)
+                    if alpha>=beta:
+                        break
+        else:
+            bestMoveValue=inf
+            for index,value in numpy.ndenumerate(self.board.cells):
+                if self.board.cells[index]=="0.0":
+                    self.board.cells[index]=self.board.players[playerIndex].value
+                    val,moveDepth=self.abpmm(depth-1,(playerIndex+1)%len(self.board.players),alpha,beta)
+                    self.board.cells[index]="0.0"
+                    if val<=bestMoveValue:
+                        if val==1 or val == 0:
+                            if moveDepth>=loseMoveDepth:
+                                bestMoveValue=val
+                                bestMoveLocation=index
+                                loseMoveDepth=moveDepth
+                                bestMoveDepth=loseMoveDepth
+                        elif val==-1:
+                            if moveDepth<=winMoveDepth:
+                                bestMoveValue=val
+                                bestMoveLocation=index
+                                winMoveDepth=moveDepth
+                                bestMoveDepth=winMoveDepth
+                    beta=min(beta,bestMoveValue)
+                    if alpha>=beta:
+                        break
+        #print(depth,val,self.board.cells,"\n")
+        return bestMoveValue,bestMoveDepth+1
+
+
+
 class MCTS:
     def __init__(self):
         pass
