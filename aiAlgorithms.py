6a5a7fb8aece5d0b3dafb8b621ccf6ae71a90ac9
Cameron Mackenzie <ckameron99@gmail.com>
2020-01-07 09:13:31 +0000

    started commenting aiAlgorithms.py

--- aiAlgorithms.py
+++ aiAlgorithms.py
@@ -1,41 +1,48 @@
 #! /usr/bin/python3
+#TODO: Inherit as much as possible
+#random is a library to generate pseudo-random numbers that I use to generate random moves
 import random
 import functools
+#operator is a module that provides pythonic operators as methods
 import operator
+#numpy is a module that I used for multi-dimensional array data structure and multi-dimensional enumeration
 import numpy
+#math provides inf which is a numerical data type which is larger than any int or float
 from math import inf
-import time
 
-class NoneAI:
+class NoneAI: #used when the game is in two player mode
     def __init__(self,board):
         pass
     def getMove(self,playerIndex):
         return False
 
-class NaiveMiniMax:
+class NaiveMiniMax: #standard minimax algorithm
     def __init__(self,board):
         self.playerIndex=None
         self.board=board
     def getMove(self,playerIndex):
+        #calculate the maximum number of moves that will be played
         maxDepth=-1
         for index, value in numpy.ndenumerate(self.board.cells):
             if self.board.cells[index]=="0.0":
                 maxDepth+=1
         self.maxDepth=maxDepth
         self.playerIndex=playerIndex
+        #as the ai is trying to maximise the reward, the current predicted reward is set to -infinity so that any move is better than that
         bestMoveValue,bestMoveLocation=-inf,None
         for index,value in numpy.ndenumerate(self.board.cells):
             if self.board.cells[index]=="0.0":
-                self.board.cells[index]=self.board.players[self.playerIndex].value
-                val=self.minimax(maxDepth,(self.playerIndex+1)%len(self.board.players))
-                self.board.cells[index]="0.0"
-                if val>=bestMoveValue:
+                self.board.cells[index]=self.board.players[self.playerIndex].value #try a particular move
+                val=self.minimax(maxDepth,(self.playerIndex+1)%len(self.board.players)) #check the expected reward of the move
+                self.board.cells[index]="0.0" #return the board to the previous state before that move
+                if val>=bestMoveValue: #if the move has a better reward than the previous best move, then reassign the best move
                     bestMoveValue=val
                     bestMoveLocation=index
         return bestMoveLocation
 
 
     def minimax(self,depth,playerIndex):
+        #start by checking if the board state gives an immediate reward
         for player in self.board.players:
             if self.board.checkWin(value=player.value):
                 if playerIndex!=self.playerIndex:
@@ -48,7 +55,8 @@ class NaiveMiniMax:
                 emptyCells+=1
         if emptyCells==0:
             return 0
-        if playerIndex==self.playerIndex:
+        #recurse another move to find the expected reward from the non-terminal board state
+        if playerIndex==self.playerIndex: #maximising, as this is the AI's turn to make a move
             bestMoveValue=-inf
             for index,value in numpy.ndenumerate(self.board.cells):
                 if self.board.cells[index]=="0.0":
@@ -56,7 +64,7 @@ class NaiveMiniMax:
                     val=self.minimax(depth-1,(playerIndex+1)%len(self.board.players))
                     self.board.cells[index]="0.0"
                     bestMoveValue=max(val,bestMoveValue)
-        else:
+        else: #minimising reward, simulating a perfect opponent
             bestMoveValue=inf
             for index,value in numpy.ndenumerate(self.board.cells):
                 if self.board.cells[index]=="0.0":
@@ -67,6 +75,7 @@ class NaiveMiniMax:
         return bestMoveValue
 
 class MiniMax:
+    # identical to naive minimax, but preferes to win quickly and lose or draw over a longer time, in order to give maximum time for sub-optimal opponent to make a mistake that improves the AI's outcome
     def __init__(self,board):
         self.playerIndex=None
         self.board=board
@@ -76,22 +85,22 @@ class MiniMax:
             if self.board.cells[index]=="0.0":
                 maxDepth+=1
         self.maxDepth=maxDepth
-        self.winMoveDepth=maxDepth
-        self.loseMoveDepth=0
+        self.winMoveDepth=maxDepth #the AI tries to minimise this
+        self.loseMoveDepth=0 #the AI tries to maximise this, and it also doubles as a drawing move depth
         self.playerIndex=playerIndex
         bestMoveValue,bestMoveLocation=-inf,None
         for index,value in numpy.ndenumerate(self.board.cells):
             if self.board.cells[index]=="0.0":
                 self.board.cells[index]=self.board.players[self.playerIndex].value
-                val,moveDepth=self.minimax(maxDepth,(self.playerIndex+1)%len(self.board.players))
+                val,moveDepth=self.minimax(maxDepth,(self.playerIndex+1)%len(self.board.players)) #also retrieve the amount of moves that will be played to achieve the reward following the move
                 self.board.cells[index]="0.0"
                 if val>=bestMoveValue:
-                    if val==-1 or val == 0:
+                    if val==-1 or val == 0: #if the AI can't currently win, then maximise the move depth
                         if moveDepth>=self.loseMoveDepth:
                             bestMoveValue=val
                             bestMoveLocation=index
                             self.loseMoveDepth=moveDepth
-                    elif val==1:
+                    elif val==1: #if the AI is going to win, then minimise the number of moves
                         if moveDepth<self.winMoveDepth:
                             bestMoveValue=val
                             bestMoveLocation=index
@@ -318,15 +327,6 @@ class MCTS:
     def __init__(self):
         pass
 
-
-class LookupTable:
-    def __init__(self):
-        pass
-
-
-class MatchBox:
-    def __init__(self):
-        pass
 
 class Random:
     def __init__(self,board):
